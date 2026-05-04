e7ed4d4691db27723b0da1360edab240b293475d
Kltvt <153986709+Kltvt@users.noreply.github.com>
2026-05-04 21:22:44 +0530

    Update speech synthesis language and voice selection
    Change default language to British English and select a voice.

--- voice.js
+++ voice.js
@@ -10,13 +10,29 @@ function speak(text) {
   window.speechSynthesis.cancel();
 
   const utterance = new SpeechSynthesisUtterance(text);
-  utterance.lang = "en-US";
+  utterance.lang = "en-GB";
   utterance.rate = 1;
   utterance.pitch = 1;
 
-  window.speechSynthesis.speak(utterance);
-}
+  const voices = speechSynthesis.getVoices();
 
+  let selectedVoice = voices.find(
+    voice => voice.name === "Google UK English Female"
+  );
+
+  // fallback to Zira
+  if (!selectedVoice) {
+    selectedVoice = voices.find(
+      voice => voice.name === "Microsoft Zira - English (United States)"
+    );
+  }
+
+  if (selectedVoice) {
+    utterance.voice = selectedVoice;
+  }
+
+  speechSynthesis.speak(utterance);
+}
 // speech recognition
 if ("webkitSpeechRecognition" in window || "SpeechRecognition" in window) {
   const SpeechRecognition =
